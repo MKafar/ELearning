0fed78963a7d8035d1b5918f28c68f87b76bee29
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-22 18:02:11 +0100

    Add ELearningDbContext

---
+++ ELearning.Persistence/ELearningDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ELearning.Domain.Entities;
+
+namespace ELearning.Persistence
+{
+    public class ELearningDbContext : DbContext
+    {
+        public ELearningDbContext(DbContextOptions<ELearningDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment> Assignments { get; set; }
+
+        public DbSet<Evaluation> Evaluations { get; set; }
+
+        public DbSet<Group> Groups { get; set; }
+
+        public DbSet<Major> Majors { get; set; }
+
+        public DbSet<Section> Sections { get; set; }
+
+        public DbSet<Subject> Subjects { get; set; }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        public DbSet<TaskVariant> TaskVariants { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ELearningDbContext).Assembly);
+        }
+    }
+}
