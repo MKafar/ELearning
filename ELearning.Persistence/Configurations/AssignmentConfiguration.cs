7fc8e4d04ce81485854b4de191047f11b7f99042
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-24 16:22:36 +0100

    Done AssignmentConfiguration

--- ELearning.Persistence/Configurations/AssignmentConfiguration.cs
+++ ELearning.Persistence/Configurations/AssignmentConfiguration.cs
@@ -6,10 +6,36 @@ namespace ELearning.Persistence.Configurations
 {
     public class AssignmentConfiguration : IEntityTypeConfiguration<Assignment>
     {
-        // TODO AssignmentConfiguration
         public void Configure(EntityTypeBuilder<Assignment> builder)
         {
-            
+            builder.HasKey(e => e.AssignmentId);
+
+            builder.Property(e => e.AssignmentId)
+                .HasColumnName("AssignmentId");
+
+            builder.Property(e => e.FinalGrade)
+                .HasColumnType("decimal(4, 2)")
+                .HasDefaultValue(0);         
+
+            builder.Property(e => e.Output)
+                .HasColumnType("ntext");
+
+            builder.Property(e => e.SectionId)
+                .HasColumnName("SectionId");
+
+            builder.Property(e => e.Solution)
+                .HasColumnType("ntext");
+
+            builder.Property(e => e.TaskVariantId)
+                .HasColumnName("TaskVariantId");
+
+            builder.HasOne(e => e.Section)
+                .WithMany(e => e.Assignments)
+                .HasConstraintName("FK_Assignments_Sections");
+
+            builder.HasOne(e => e.TaskVariant)
+                .WithMany(e => e.Assignments)
+                .HasConstraintName("FK_Assignments_TaskVariants");
         }
     }
 }
