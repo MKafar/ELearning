108e2158d07d9e79d4b84f17b7bbc048bc5f7038
Daria Ratyńska <ratynskadaria@gmail.com>
2018-12-27 23:26:29 +0100

    working detailList module, need to fix searchTable module

--- ELearning.WebUI/ClientApp/src/App.js
+++ ELearning.WebUI/ClientApp/src/App.js
@@ -1,25 +1,51 @@
-import React, { Component } from 'react'
-import 'semantic-ui-css/semantic.min.css'
-import 'semantic-ui-react'
+import React, { Component } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-react';
 
-import './App.css'
-import Menu from './Menu/Menu.jsx'
-import CodeMirror from './CodeMirror/CodeMirror.jsx'
+import './App.css';
+import Menu from './Menu/Menu.jsx';
+import SearchTable from './Modules/SearchTable.jsx';
 
 class App extends Component {
+
+
+  state = {
+    exercises: [
+      { number: 1, title: 'Jakiś tytuł ćwiczenia' },
+      { number: 2, title: 'B' },
+      { number: 3, title: 'C' },
+      { number: 4, title: 'D' },
+      { number: 5, title: 'E' },
+      { number: 6, date: '01.01.18', title: 'Kolejny długaśny bardzo długi tytuł, no bardzo długi ffffffffffffffffff ffffffffffffffffffffffff' }
+    ],
+    searches: [
+      { date: '01.02.18', lab: 'PP', exercise: 'A', student: 'Mateusz M'},
+      { date: '02.02.18', lab: 'PP', exercise: 'B', student: 'Mateusz A'},
+      { date: '03.02.18', lab: 'PP', exercise: 'A', student: 'Mateusz K'}
+    ]
+  }
+
+
+
   render() {
+
     return (
       <div className='App'>
-        <div className =" block">
+        <div className=" block">
           <div className="menu">
             <Menu></Menu>
           </div>
           <div>
-            <CodeMirror />
+         { this.state.searches.map((search) => {
+           return <SearchTable
+           date = { search.date }
+           lab = { search.lab }
+           exercise = { search.exercise }
+           student = { search.student } />
+           })}
           </div>
 
         </div>
-        
 
 
 
@@ -29,6 +55,14 @@ class App extends Component {
 
 
 
+        {/*  <div className = 'detailList'>
+            {this.state.exercises.map((exercise) => {
+              return <DetailList
+                number = { exercise.number }
+                title = { exercise.title }
+                date = { exercise.date }/>
+            } )}
+          </div> */}
 
 
         {/*
--- ELearning.WebUI/ClientApp/src/Modules/DeatilList.jsx
+++ ELearning.WebUI/ClientApp/src/Modules/DeatilList.jsx
@@ -1,30 +1,39 @@
-import React, { Component } from 'react'
-import { Container, List } from 'semantic-ui-react'
+import React from 'react'
+import { Container, List, Button } from 'semantic-ui-react'
 
 import './DetailList.scss'
 
-class DetailList extends Component {
-    
-    redner() {
-        return (
-            <div>
-                <Container>
-                    <List divided relaxed>
-                        <List.Item>
-                            <List.Content floated='right'>
-                                Szczegóły
-                </List.Content>
-                            <List.Content>
-                                Zadanie
-                </List.Content>
-                        </List.Item>
-                    </List>
-                </Container>
 
-            </div>
+const DetailList = (props) => {
 
-        )
+    const detailsHandler = () => {
+        console.log('Szczegóły', props.number);
     }
+
+    return (
+        <div>
+            <Container>
+                <List divided relaxed>
+                    <List.Item>
+                        <List.Content floated='right'>
+                            <Button onClick={detailsHandler}> Szczegóły </Button>
+                        </List.Content>
+                        <List.Content floated='left'>
+                            { props.number }
+                        </List.Content>
+                        <List.Content  floated='left'>
+                            { props.date }
+                        </List.Content>
+                        <List.Content >
+                            { props.title }
+                        </List.Content>
+                    </List.Item>
+                </List>
+            </Container>
+
+        </div>
+
+    );
 }
 
 export default DetailList;
