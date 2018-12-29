62bd637ab9fb0fa81f3320e7d079d7f4ff7c1258
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-29 21:04:37 +0100

    Custom exceptions

---
+++ ELearning.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ELearning.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+        }
+    }
+}
