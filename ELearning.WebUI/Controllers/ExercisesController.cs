07bac7ad69e1101bdfef6a12c7850a96af135ec2
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-29 21:20:21 +0100

    Rename TasksController to ExercisesController

---
+++ ELearning.WebUI/Controllers/ExercisesController.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using ELearning.Application.Exercises.Commands.CreateExercise;
+using ELearning.Application.Exercises.Commands.DeleteExercise;
+using ELearning.Application.Exercises.Commands.UpdateExercise;
+using ELearning.Application.Exercises.Queries.GetExerciseById;
+using ELearning.Application.Exercises.Queries.GetExercisesList;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ELearning.WebUI.Controllers
+{
+    public class ExercisesController : BaseController
+    {
+        // GET: api/tasks/GetAll
+        [HttpGet]
+        public async Task<ActionResult<ExercisesListViewModel>> GetAll()
+        {
+            return Ok(await Mediator.Send(new GetExercisesListQuery()));
+        }
+
+        // GET api/tasks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ExerciseViewModel>> GetById(int id)
+        {
+            return Ok(await Mediator.Send(new GetExerciseByIdQuery { Id = id }));
+        }
+
+        // POST api/tasks
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public async Task<IActionResult> Create([FromBody]CreateExerciseCommand command)
+        {
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        // PUT api/tasks/5
+        [HttpPut("{id}")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public async Task<IActionResult> Update(int id, [FromBody]UpdateExerciseCommand command)
+        {
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        // DELETE api/tasks/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await Mediator.Send(new DeleteExerciseCommand { Id = id });
+
+            return NoContent();
+        }
+    }
+}
