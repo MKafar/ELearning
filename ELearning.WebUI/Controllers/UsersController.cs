845cf4b2912b1076fe7f2a28b5391e4a227a5cb0
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-30 14:20:48 +0100

    Add UserController

---
+++ ELearning.WebUI/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using ELearning.Application.Users.Queries.GetUsersList;
+using ELearning.Application.Users.Queries.GetUserById;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ELearning.WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : BaseController
+    {
+        // GET: api/users
+        [HttpGet]
+        public async Task<ActionResult<UsersListViewModel>> GetAll()
+        {
+            return Ok(await Mediator.Send(new GetUsersListQuery()));
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserViewModel>> Get(int id)
+        {
+            return Ok(await Mediator.Send(new GetUserByIdQuery { Id = id }));
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
