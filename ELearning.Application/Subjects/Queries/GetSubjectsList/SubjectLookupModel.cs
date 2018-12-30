eac145048582d35db09bd17929bf6be13607b07e
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-30 22:18:31 +0100

    Add GetSubjectsListQuery with handler and validator

---
+++ ELearning.Application/Subjects/Queries/GetSubjectsList/SubjectLookupModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ELearning.Application.Subjects.Queries.GetSubjectsList
+{
+    public class SubjectLookupModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Abreviation { get; set; }
+    }
+}
