4720bc8808f8125cf1d93c582677c471dd82132a
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-30 22:17:41 +0100

    Add CreateSubjectCommand with handler and validator

---
+++ ELearning.Application/Subjects/Command/CreateSubject/CreateSubjectCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ELearning.Domain.Entities;
+using ELearning.Persistence;
+using MediatR;
+
+namespace ELearning.Application.Subjects.Command.CreateSubject
+{
+    public class CreateSubjectCommandHandler : IRequestHandler<CreateSubjectCommand, Unit>
+    {
+        private readonly ELearningDbContext _context;
+
+        public CreateSubjectCommandHandler(ELearningDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(CreateSubjectCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new Subject
+            {
+                Name = request.Name,
+                Abreviation = request.Abreviation
+            };
+
+            _context.Subjects.Add(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ ELearning.Application/Subjects/Command/CreateSubject/CreateSubjectCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace ELearning.Application.Subjects.Command.CreateSubject
+{
+    public class CreateSubjectCommandValidator : AbstractValidator<CreateSubjectCommand>
+    {
+        public CreateSubjectCommandValidator()
+        {
+            RuleFor(v => v.Name)
+                .NotEmpty()
+                .MaximumLength(128);
+
+            RuleFor(v => v.Abreviation)
+                .NotEmpty()
+                .MaximumLength(8);
+        }
+    }
+}
