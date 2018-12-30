e3bfa7230f9d204eb31aebb537c0df46e5584875
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-30 22:18:02 +0100

    Add UpdateSubjectCommand with handler and validator

---
+++ ELearning.Application/Subjects/Command/UpdateSubject/UpdateSubjectCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace ELearning.Application.Subjects.Command.UpdateSubject
+{
+    public class UpdateSubjectCommand : IRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Abreviation { get; set; }
+    }
+}
---
+++ ELearning.Application/Subjects/Command/UpdateSubject/UpdateSubjectCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace ELearning.Application.Subjects.Command.UpdateSubject
+{
+    public class UpdateSubjectCommandValidator : AbstractValidator<UpdateSubjectCommand>
+    {
+        public UpdateSubjectCommandValidator()
+        {
+            RuleFor(v => v.Id)
+                .NotEmpty()
+                .GreaterThan(0);
+
+            RuleFor(v => v.Name)
+                .NotEmpty()
+                .MaximumLength(128);
+
+            RuleFor(v => v.Abreviation)
+                .NotEmpty()
+                .MaximumLength(8);
+        }
+    }
+}
