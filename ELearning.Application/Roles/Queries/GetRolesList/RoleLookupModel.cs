5855b7b1a4eacdaf29fca3738f0f7c0d3e6d1df5
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-31 00:52:35 +0100

    Add GetRolesList query

---
+++ ELearning.Application/Roles/Queries/GetRolesList/RoleLookupModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ELearning.Application.Roles.Queries.GetRolesList
+{
+    public class RoleLookupModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
