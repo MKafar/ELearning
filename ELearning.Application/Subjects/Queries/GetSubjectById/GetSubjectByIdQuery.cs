3b1c7918f1293fcdd56390fcb89f51b7ff48416f
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-30 22:18:49 +0100

    Add GetSubjectByIdQuery with handler and validator

---
+++ ELearning.Application/Subjects/Queries/GetSubjectById/GetSubjectByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ELearning.Application.Subjects.Queries.GetSubjectById
+{
+    public class GetSubjectByIdQuery : IRequest<SubjectViewModel>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ ELearning.Application/Subjects/Queries/GetSubjectById/GetSubjectByIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ELearning.Application.Subjects.Queries.GetSubjectById
+{
+    public class GetSubjectByIdQueryValidator : AbstractValidator<GetSubjectByIdQuery>
+    {
+        public GetSubjectByIdQueryValidator()
+        {
+            RuleFor(v => v.Id)
+                .NotEmpty()
+                .GreaterThan(0);
+        }
+    }
+}
