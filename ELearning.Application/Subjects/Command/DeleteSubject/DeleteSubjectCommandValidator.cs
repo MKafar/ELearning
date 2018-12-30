c185433c20ce7979ec80497c8c798b68a1915ec2
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-30 22:17:52 +0100

    Add DeleteSubjectCommand with handler and validator

---
+++ ELearning.Application/Subjects/Command/DeleteSubject/DeleteSubjectCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ELearning.Application.Subjects.Command.DeleteSubject
+{
+    public class DeleteSubjectCommandValidator : AbstractValidator<DeleteSubjectCommand>
+    {
+        public DeleteSubjectCommandValidator()
+        {
+            RuleFor(v => v.Id)
+                .NotEmpty()
+                .GreaterThan(0);
+        }
+    }
+}
