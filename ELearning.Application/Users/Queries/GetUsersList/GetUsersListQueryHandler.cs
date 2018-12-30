f339d35eb8c2b786aad2f26b0114db3cc1cc6815
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-30 14:21:14 +0100

    Add GetUserList query

---
+++ ELearning.Application/Users/Queries/GetUsersList/GetUsersListQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using ELearning.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ELearning.Application.Users.Queries.GetUsersList
+{
+    public class GetUsersListQueryHandler : IRequestHandler<GetUsersListQuery, UsersListViewModel>
+    {
+        private readonly ELearningDbContext _context;
+
+        public GetUsersListQueryHandler(ELearningDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UsersListViewModel> Handle(GetUsersListQuery request, CancellationToken cancellationToken)
+        {
+            var vm = new UsersListViewModel
+            {
+                Users = await _context.Users
+                    .Select(e => new UserLookupModel
+                    {
+                        Id = e.UserId,
+                        Name = e.Name,
+                        Surname = e.Surname
+                    }).ToListAsync(cancellationToken)
+            };
+
+            return vm;
+        }
+    }
+}
---
+++ ELearning.Application/Users/Queries/GetUsersList/UserLookupModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ELearning.Application.Users.Queries.GetUsersList
+{
+    public class UserLookupModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
