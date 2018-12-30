6a1dc6646f6f17e122606709a4270240e1554829
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-31 00:52:21 +0100

    Add GetRoleById query

---
+++ ELearning.Application/Roles/Queries/GetRoleById/GetRoleByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ELearning.Application.Roles.Queries.GetRoleById
+{
+    public class GetRoleByIdQuery : IRequest<RoleViewModel>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ ELearning.Application/Roles/Queries/GetRoleById/GetRoleByIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ELearning.Application.Roles.Queries.GetRoleById
+{
+    public class GetRoleByIdQueryValidator : AbstractValidator<GetRoleByIdQuery>
+    {
+        public GetRoleByIdQueryValidator()
+        {
+            RuleFor(v => v.Id)
+                .NotEmpty()
+                .GreaterThan(0);
+        }
+    }
+}
