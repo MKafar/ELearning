23276951fe8f4236ea0c3fa1b471d6c0ca96492a
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-30 20:30:53 +0100

    Add CreateUserCommand

---
+++ ELearning.Application/Users/Command/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace ELearning.Application.Users.Command.CreateUser
+{
+    public class CreateUserCommand : IRequest
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public int RoleId { get; set; }
+    }
+}
