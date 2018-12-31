ab44872e3b74fc2d4507cf7f35482b4c8fa1944d
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-31 01:57:09 +0100

    Add GetGroupsList query

---
+++ ELearning.Application/Groups/Queries/GetGroupsList/GetGroupsListQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ELearning.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ELearning.Application.Groups.Queries.GetGroupsList
+{
+    public class GetGroupsListQueryHandler : IRequestHandler<GetGroupsListQuery, GroupsListViewModel>
+    {
+        private readonly ELearningDbContext _context;
+
+        public GetGroupsListQueryHandler(ELearningDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GroupsListViewModel> Handle(GetGroupsListQuery request, CancellationToken cancellationToken)
+        {
+            var vm = new GroupsListViewModel
+            {
+                Groups = await _context.Groups
+                    .Select(e => new GroupLookupModel
+                    {
+                        Name = e.Name,
+                        AcademicYear = e.AcademicYear,
+                        SubjectName = e.Subject.Name,
+                        SubjectAbreviation = e.Subject.Abreviation
+                    }).ToListAsync(cancellationToken)
+            };
+
+            return vm;
+        }
+    }
+}
---
+++ ELearning.Application/Groups/Queries/GetGroupsList/GroupLookupModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ELearning.Application.Groups.Queries.GetGroupsList
+{
+    public class GroupLookupModel
+    {
+        public string Name { get; set; }
+        public short AcademicYear { get; set; }
+        public string SubjectName { get; set; }
+        public string SubjectAbreviation { get; set; }
+    }
+}
