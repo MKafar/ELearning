4c45e6268d19382b3ffe5224b664a4d158733394
Mateusz <mm.mateusz.mlotek@gmail.com>
2018-12-31 19:52:35 +0100

    Add CreateGroup

---
+++ ELearning.Application/Groups/Commands/CreateGroup/CreateGroupCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ELearning.Domain.Entities;
+using ELearning.Persistence;
+using MediatR;
+
+namespace ELearning.Application.Groups.Commands.CreateGroup
+{
+    public class CreateGroupCommandHandler : IRequestHandler<CreateGroupCommand, Unit>
+    {
+        private readonly ELearningDbContext _context;
+
+        public CreateGroupCommandHandler(ELearningDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(CreateGroupCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new Group
+            {
+                Name = request.Name,
+                AcademicYear = request.AcademicYear,
+                SubjectId = request.SubjectId
+            };
+
+            _context.Groups.Add(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ ELearning.Application/Groups/Commands/CreateGroup/CreateGroupCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+
+namespace ELearning.Application.Groups.Commands.CreateGroup
+{
+    public class CreateGroupCommandValidator : AbstractValidator<CreateGroupCommand>
+    {
+        public CreateGroupCommandValidator()
+        {
+            RuleFor(v => v.Name)
+                .MaximumLength(30);
+
+            RuleFor(v => v.AcademicYear)
+                .LessThanOrEqualTo(DateTime.Now.Year);
+
+            RuleFor(v => v.SubjectId)
+                .NotEmpty()
+                .GreaterThan(0);
+        }
+    }
+}
