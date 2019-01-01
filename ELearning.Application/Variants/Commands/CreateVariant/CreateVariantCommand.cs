f41865cf2403229c4a1fc9bed7f0b6d4ec36ea15
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-02 00:51:24 +0100

    Add CreateVariantCommand

---
+++ ELearning.Application/Variants/Commands/CreateVariant/CreateVariantCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace ELearning.Application.Variants.Commands.CreateVariant
+{
+    public class CreateVariantCommand : IRequest
+    {
+        public string Content { get; set; }
+        public int Number { get; set; }
+        public int ExerciseId { get; set; }
+    }
+}
