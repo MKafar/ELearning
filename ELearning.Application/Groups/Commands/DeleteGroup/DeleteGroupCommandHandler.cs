25cc10551a1496bee54f0d8620e64d87b5add9cd
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-01 18:25:02 +0100

    Add DeleteGroup command

---
+++ ELearning.Application/Groups/Commands/DeleteGroup/DeleteGroupCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ELearning.Application.Exceptions;
+using ELearning.Domain.Entities;
+using ELearning.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ELearning.Application.Groups.Commands.DeleteGroup
+{
+    public class DeleteGroupCommandHandler : IRequestHandler<DeleteGroupCommand, Unit>
+    {
+        private readonly ELearningDbContext _context;
+
+        public DeleteGroupCommandHandler(ELearningDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteGroupCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Groups
+                .FindAsync(request.Id);
+
+            var entity2 = await _context.Groups
+                .SingleOrDefaultAsync(e => e.GroupId == 500);
+
+            if (entity == null)
+                throw new NotFoundException(nameof(Group), request.Id);
+
+            var hasSections = _context.Sections.Any(e => e.GroupId == entity.GroupId);
+
+            if (hasSections)
+                throw new DeleteFailureException(nameof(Group), request.Id, "There are existing sections associated with this group.");
+
+            _context.Groups.Remove(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ ELearning.Application/Groups/Commands/DeleteGroup/DeleteGroupCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ELearning.Application.Groups.Commands.DeleteGroup
+{
+    public class DeleteGroupCommandValidator : AbstractValidator<DeleteGroupCommand>
+    {
+        public DeleteGroupCommandValidator()
+        {
+            RuleFor(v => v.Id)
+                .NotEmpty()
+                .GreaterThan(0);
+        }
+    }
+}
