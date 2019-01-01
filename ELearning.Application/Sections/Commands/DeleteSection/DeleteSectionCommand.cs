54725d7a51b97284fec76c8c8292366636633fc8
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-01 23:20:01 +0100

    Add DeleteSectionCommand

---
+++ ELearning.Application/Sections/Commands/DeleteSection/DeleteSectionCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ELearning.Application.Sections.Commands.DeleteSection
+{
+    public class DeleteSectionCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ ELearning.Application/Sections/Commands/DeleteSection/DeleteSectionCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ELearning.Application.Sections.Commands.DeleteSection
+{
+    public class DeleteSectionCommandValidator : AbstractValidator<DeleteSectionCommand>
+    {
+        public DeleteSectionCommandValidator()
+        {
+            RuleFor(v => v.Id)
+                .NotEmpty()
+                .GreaterThan(0);
+        }
+    }
+}
