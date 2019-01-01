d2f5d0cc118a95c624a1a78714d7a7d3e8715ea8
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-02 00:51:32 +0100

    Add DeleteVariantCommand

---
+++ ELearning.Application/Variants/Commands/DeleteVariant/DeleteVariantCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ELearning.Application.Variants.Commands.DeleteVariant
+{
+    public class DeleteVariantCommandValidator : AbstractValidator<DeleteVariantCommand>
+    {
+        public DeleteVariantCommandValidator()
+        {
+            RuleFor(v => v.Id)
+                .NotEmpty()
+                .GreaterThan(0);
+        }
+    }
+}
