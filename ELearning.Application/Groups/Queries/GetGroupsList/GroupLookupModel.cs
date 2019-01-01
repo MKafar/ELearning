1ad0bef94a806e24923b17c3287ed44399d601fd
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-01 18:25:55 +0100

    Fix group models. AcademicYear data type short -> int

--- ELearning.Application/Groups/Queries/GetGroupsList/GroupLookupModel.cs
+++ ELearning.Application/Groups/Queries/GetGroupsList/GroupLookupModel.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public short AcademicYear { get; set; }
+        public int AcademicYear { get; set; }
         public int SubjectId { get; set; }
         public string SubjectName { get; set; }
         public string SubjectAbreviation { get; set; }
