a6a45768aa8fc620bd9fb00653ec812e34796820
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-01 23:19:53 +0100

    Add CreateSectionCommand

---
+++ ELearning.Application/Sections/Commands/CreateSection/CreateSectionCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace ELearning.Application.Sections.Commands.CreateSection
+{
+    public class CreateSectionCommandValidator : AbstractValidator<CreateSectionCommand>
+    {
+        public CreateSectionCommandValidator()
+        {
+            RuleFor(v => v.GroupId)
+                .NotEmpty()
+                .GreaterThan(0);
+
+            RuleFor(v => v.UserId)
+                .NotEmpty()
+                .GreaterThan(0);
+
+            RuleFor(v => v.Number)
+                .NotEmpty()
+                .GreaterThan(0)
+                .LessThanOrEqualTo(32767);
+        }
+    }
+}
