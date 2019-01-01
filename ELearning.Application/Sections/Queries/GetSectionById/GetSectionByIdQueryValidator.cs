1635ae08ffe7cc1a508cac1576fd5f1731c45855
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-01 23:20:22 +0100

    Add GetSectionByIdQuery

---
+++ ELearning.Application/Sections/Queries/GetSectionById/GetSectionByIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ELearning.Application.Sections.Queries.GetSectionById
+{
+    public class GetSectionByIdQueryValidator : AbstractValidator<GetSectionByIdQuery>
+    {
+        public GetSectionByIdQueryValidator()
+        {
+            RuleFor(v => v.Id)
+                .NotEmpty()
+                .GreaterThan(0);
+        }
+    }
+}
