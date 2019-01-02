d6502b4685c677cca6a23b0570f70291958dc734
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-02 14:27:01 +0100

    Add GetEvaluationsListBySectionIdQuery

---
+++ ELearning.Application/Evaluations/Queries/GetEvaluationsListBySectionId/GetEvaluationsListBySectionIdQueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ELearning.Application.Evaluations.Models;
+using ELearning.Application.Exceptions;
+using ELearning.Domain.Entities;
+using ELearning.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ELearning.Application.Evaluations.Queries.GetEvaluationsListBySectionId
+{
+    public class GetEvaluationsListBySectionIdQueryHandler : IRequestHandler<GetEvaluationsListBySectionIdQuery, EvaluationsListViewModel>
+    {
+        private readonly ELearningDbContext _context;
+
+        public GetEvaluationsListBySectionIdQueryHandler(ELearningDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<EvaluationsListViewModel> Handle(GetEvaluationsListBySectionIdQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Evaluations
+                .Select(e => new EvaluationLookupModel
+                {
+                    Id = e.EvaluationId,
+                    AssignmentId = e.AssignmentId,
+                    SectionId = e.SectionId,
+                    Grade = e.Grade
+                }).Where(e => e.SectionId == request.Id)
+                .ToListAsync(cancellationToken);
+
+            if (entity == null)
+                throw new NotFoundException(nameof(Evaluation), request.Id);
+
+            return new EvaluationsListViewModel
+            {
+                Evaluations = entity
+            };
+        }
+    }
+}
