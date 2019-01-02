f748f1759eca841468623c443c3ce881f7a59eff
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-02 14:26:44 +0100

    Add GetEvaluationsListByAssignmentIdQuery

---
+++ ELearning.Application/Evaluations/Queries/GetEvaluationsListByAssignmentId/GetEvaluationsListByAssignmentIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ELearning.Application.Evaluations.Queries.GetEvaluationsListByAssignmentId
+{
+    public class GetEvaluationsListByAssignmentIdQueryValidator : AbstractValidator<GetEvaluationsListByAssignmentIdQuery>
+    {
+        public GetEvaluationsListByAssignmentIdQueryValidator()
+        {
+            RuleFor(v => v.Id)
+                .NotEmpty()
+                .GreaterThan(0);
+        }
+    }
+}
