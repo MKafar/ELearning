735fcaad93ec12590cd3d3048f982927630c0e12
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-02 12:50:04 +0100

    Add ParseDateFailureException custom exception

---
+++ ELearning.Application/Exceptions/ParseDateFailureException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ELearning.Application.Exceptions
+{
+    public class ParseDateFailureException : Exception
+    {
+        public ParseDateFailureException(string date)
+            :base ($"Parsing of date {date} was not successful.")
+        {
+        }
+    }
+}
