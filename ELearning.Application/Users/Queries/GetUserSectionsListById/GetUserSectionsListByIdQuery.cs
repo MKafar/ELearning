63cb53de6775050926bfea26a0b923a5675465ec
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-02 16:44:31 +0100

    Add GetUserSectionsListByIdQuery

---
+++ ELearning.Application/Users/Queries/GetUserSectionsListById/GetUserSectionsListByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ELearning.Application.Users.Queries.GetUserSectionsListById
+{
+    public class GetUserSectionsListByIdQuery : IRequest<UserSectionsListViewModel>
+    {
+        public int Id { get; set; }
+    }
+}
