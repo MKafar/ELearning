971ebe2a38f1f86ad73d36b051ba0bb0bdefe0d5
Daria Ratyńska <ratynskadaria@gmail.com>
2019-01-02 12:48:32 +0100

    done StudentDetails

--- ELearning.WebUI/ClientApp/src/containers/Admin/StudentDetails.jsx
+++ ELearning.WebUI/ClientApp/src/containers/Admin/StudentDetails.jsx
@@ -7,9 +7,9 @@ import DetailList from '../../components/Modules/DetailList';
 class StudentDetails extends Component {
     state = {
         studentExercises: [
-            { id: 1, title: 'Tytuł zadania', subject: 'PP', date: '01.01.2019', grade: 3.5 },
-            { id: 2, title: 'Tytuł zadania', subject: 'PP', date: '01.01.2019', grade: 4.0 },
-            { id: 3, title: 'Tytuł zadania', subject: 'PP', date: '01.01.2019', grade: 5.0 },
+            { id: 1, title: 'Tytuł zadania', subject: 'PP', date: '01.01.2019', grade: 3.5, variant: 1 },
+            { id: 2, title: 'Tytuł zadania', subject: 'PP', date: '01.01.2019', grade: 4.0, variant: 2 },
+            { id: 3, title: 'Tytuł zadania', subject: 'PP', date: '01.01.2019', grade: 5.0, variant: 3 },
         ],
         subjectOptions: [
             { key: 1, text: 'Choice 1', value: 1 },
@@ -29,41 +29,45 @@ class StudentDetails extends Component {
                     <Header size='large'>Imię i nazwisko studenta</Header>
                 </div>
                 <div className='studentTables'>
-                    <div>
+                    <div className='assignmentTable'>
                         {this.state.studentExercises.map((studentExercise) => {
                             return <DetailList
                                 visibledetail={true}
                                 visibledelete={false}
                                 key={studentExercise.id}
                                 title={studentExercise.title}
-                                subject={studentExercise.subject}
+                                subject={"Przedmiot: " + studentExercise.subject}
                                 date={studentExercise.date}
+                                variant={'Wariant: ' + studentExercise.variant + " "}
                                 grade={"Ocena: " + studentExercise.grade} />
                         })}
                     </div>
-                    <div>
-                    <Dropdown className='studentInput' placeholder='Laboratorium' clearable options={this.subjectOptions} selection />
-                    <Dropdown className='studentInput' placeholder='Grupa' clearable options={this.groupOptions} selection />
-                    <Input className='sectionInput' placeholder='Sekcja' onChange={this.sectionHandle} />
-                    <Button className='addtolistbutton' primary onClick={this.addtolistHandle}>Dodaj</Button>
+                    <div className='assignGroup'>
+                        <div className='assignInputs'>
+                            <Dropdown className='studentInputAssignment' placeholder='Laboratorium' clearable options={this.state.subjectOptions} selection />
+                            <Dropdown className='studentInputAssignment' placeholder='Grupa' clearable options={this.state.groupOptions} selection />   
+                            <Input className='sectionInputAssignment' placeholder='Sekcja' onChange={this.sectionHandle} />
+                            <br />
+                            <Button className='addtolistbuttonAssignment' primary onClick={this.addtolistHandle}>Dodaj</Button>
+                        </div>
 
-                    <Table basic='very' className='addstudenttable'>
-                        <Table.Header>
-                            <Table.Row>
-                                <Table.HeaderCell>Grupa</Table.HeaderCell>
-                                <Table.HeaderCell>Sekcja</Table.HeaderCell>
-                                <Table.HeaderCell></Table.HeaderCell>
-                            </Table.Row>
-                        </Table.Header>
+                        <Table basic='very' className='addstudenttableAssignment'>
+                            <Table.Header>
+                                <Table.Row>
+                                    <Table.HeaderCell>Grupa</Table.HeaderCell>
+                                    <Table.HeaderCell>Sekcja</Table.HeaderCell>
+                                    <Table.HeaderCell></Table.HeaderCell>
+                                </Table.Row>
+                            </Table.Header>
 
-                        <Table.Body>
-                            <Table.Row>
-                                <Table.Cell className='group'>Approved</Table.Cell>
-                                <Table.Cell>None</Table.Cell>
-                                <Table.Cell><Button className='deletetablebutton'>Usuń</Button></Table.Cell>
-                            </Table.Row>
-                        </Table.Body>
-                    </Table>
+                            <Table.Body>
+                                <Table.Row>
+                                    <Table.Cell className='group'>Approved</Table.Cell>
+                                    <Table.Cell>None</Table.Cell>
+                                    <Table.Cell><Button className='deletetablebutton'>Usuń</Button></Table.Cell>
+                                </Table.Row>
+                            </Table.Body>
+                        </Table>
                     </div>
                 </div>
             </div>
