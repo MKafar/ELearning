bebf4eb9dadb722569ec3b01d9ead7856872d49b
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-02 12:50:35 +0100

    Add DeleteAssignmentCommand

---
+++ ELearning.Application/Assignments/Commands/DeleteAssignment/DeleteAssignmentCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ELearning.Application.Assignments.Commands.DeleteAssignment
+{
+    public class DeleteAssignmentCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
