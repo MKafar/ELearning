dfa64be15213b0d652e5d985ea67e7991a168aef
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-03 12:08:59 +0100

    Add compiler endpoint, controller and service

---
+++ ELearning.Application/Compiler/Commands/CompileCode/CompileCodeCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace ELearning.Application.Compiler.Commands.CompileCode
+{
+    public class CompileCodeCommand : IRequest<CompilerOutputViewModel>
+    {
+        public int AssignmentId { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ ELearning.Application/Interfaces/ICompilerService.cs
@@ -0,0 +1,7 @@
+﻿namespace ELearning.Application.Interfaces
+{
+    public interface ICompilerService
+    {
+        void Compile();
+    }
+}
