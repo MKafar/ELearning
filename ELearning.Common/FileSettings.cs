09283de6e42974dc42af8975178a245f03530f8f
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-03 19:29:08 +0100

    Add FileSettings class to ELearning.Common

---
+++ ELearning.Common/FileSettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace ELearning.Common
+{
+    public class FileSettings
+    {
+        public string FilePath
+        {
+            get
+            {
+                if (FileSaveDirectory == null)
+                    throw new ArgumentNullException(nameof(FileSaveDirectory));
+
+                if (FileName == null)
+                    throw new ArgumentNullException(nameof(FileName));
+
+                return string.Format("{0}{1}{2}",
+                    FileSaveDirectory, 
+                    Path.DirectorySeparatorChar, 
+                    FileName);
+            }
+        }
+
+        public string FileSaveDirectory { get; set; } = null;
+        public string FileName { get; set; } = null;
+
+        public string FileNameWithoutExtension
+        {
+            get
+            {
+                if (FileName == null)
+                    throw new ArgumentNullException(nameof(FileName));
+
+                return Path.ChangeExtension(FileName, "").Substring(0, FileName.LastIndexOf('.'));
+            }
+        }
+    }
+}
