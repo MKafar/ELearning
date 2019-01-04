7a0bcb4e1972e793102dcb863e2a8dd8ae3e0c32
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-04 01:57:14 +0100

    Fix CompileCodeCommand

--- ELearning.Application/Compiler/Commands/CompileCode/CompileCodeCommandHandler.cs
+++ ELearning.Application/Compiler/Commands/CompileCode/CompileCodeCommandHandler.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using ELearning.Application.Exceptions;
 using ELearning.Application.Interfaces;
-using ELearning.Common;
+using ELearning.Domain.Entities;
+using ELearning.Persistence;
 using MediatR;
 
 namespace ELearning.Application.Compiler.Commands.CompileCode
 {
     public class CompileCodeCommandHandler : IRequestHandler<CompileCodeCommand, CompilerOutputViewModel>
     {
+        private readonly ELearningDbContext _context;
         private readonly ICompilerService _compiler;
         private readonly IFileSaveService _filesave;
         private readonly DateTime _now;
 
-        public CompileCodeCommandHandler(ICompilerService compiler, IFileSaveService filesave)
+        public CompileCodeCommandHandler(ELearningDbContext context, ICompilerService compiler, IFileSaveService filesave)
         {
+            _context = context;
             _compiler = compiler;
             _filesave = filesave;
             _now = DateTime.Now;
@@ -22,6 +26,12 @@ namespace ELearning.Application.Compiler.Commands.CompileCode
 
         public async Task<CompilerOutputViewModel> Handle(CompileCodeCommand request, CancellationToken cancellationToken)
         {
+            var entity = await _context.Assignments
+                .FindAsync(request.AssignmentId);
+
+            if (entity == null)
+                throw new NotFoundException(nameof(Assignment), request.AssignmentId);
+
             var fileSettings = await _filesave.SaveToFileAsync(request.AssignmentId, request.Code, _now, cancellationToken);
 
             var output = await _compiler.CompileAsync(request.Code, fileSettings, cancellationToken);
