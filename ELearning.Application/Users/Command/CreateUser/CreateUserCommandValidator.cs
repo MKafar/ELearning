b1687f2109e2d29084b2dabb5a7d939de889a32d
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-04 12:12:21 +0100

    Fix CreateUserCommandValidator to accept polish signs in user name or surname

--- ELearning.Application/Users/Command/CreateUser/CreateUserCommandValidator.cs
+++ ELearning.Application/Users/Command/CreateUser/CreateUserCommandValidator.cs
@@ -9,19 +9,19 @@ namespace ELearning.Application.Users.Command.CreateUser
             RuleFor(v => v.Name)
                 .NotEmpty()
                 .MaximumLength(50)
-                .Matches("^[A-Z]([a-z0-9_-]{1,}|)$")
+                .Matches("^[A-ZĄĆĘŁŃÓŚŹŻ]([a-ząćęłńóśźż0-9_-]{1,}|)$")
                 .WithMessage("Name should have one upper case letter at the start.");
 
             RuleFor(v => v.Surname)
                 .NotEmpty()
                 .MaximumLength(50)
-                .Matches("^[A-Z]([a-z0-9_-]{1,}|)$")
+                .Matches("^[A-ZĄĆĘŁŃÓŚŹŻ]([a-ząćęłńóśźż0-9_-]{1,}|)$")
                 .WithMessage("Surname should have one upper case letter at the start.");
 
             RuleFor(v => v.Email)
                 .NotEmpty()
                 .MaximumLength(320)
-                .Matches("^.{1,64}@(student.|)polsl.pl$")
+                .Matches("^.{1,64}@student.polsl.pl$")
                 .WithMessage("University's email has format {login@student.polsl.pl} or {name.surname@student.polsl.pl}.");
         }
     }
