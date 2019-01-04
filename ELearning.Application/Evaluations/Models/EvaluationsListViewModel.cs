2a13517b62bbc04cdd94e060f3bab1012e74f708
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-04 16:50:02 +0100

    Remove evaluations query GetEvaluationsListByAssignmentId and GetEvaluationsListBySectionId. Remove unnecessary models

--- ELearning.Application/Evaluations/Models/EvaluationsListViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace ELearning.Application.Evaluations.Models
-{
-    public class EvaluationsListViewModel
-    {
-        public IList<EvaluationLookupModel> Evaluations { get; set; }
-    }
-}
