99a696b87031704c8490ed8d9678fa8026166ab5
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-04 18:25:10 +0100

    Change NotUniqueException message.

--- ELearning.Application/Exceptions/NotUniqueException.cs
+++ ELearning.Application/Exceptions/NotUniqueException.cs
@@ -5,7 +5,7 @@ namespace ELearning.Application.Exceptions
     public class NotUniqueException : Exception
     {
         public NotUniqueException(string entity, string property1, int value1, string property2, int value2)
-            : base($"{entity} with {property1} equal {value1} and {property2} equal {value2} already exists.")
+            : base($"{entity} with {property1}: {value1} and {property2}: {value2} already exists.")
         {
         }
     }
