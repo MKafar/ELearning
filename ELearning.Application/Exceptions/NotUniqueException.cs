a7ec11a430fbf829ddeaef47d239487eb588f2f4
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-04 18:19:23 +0100

    Fix CreateVariantCommand unique values

---
+++ ELearning.Application/Exceptions/NotUniqueException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ELearning.Application.Exceptions
+{
+    public class NotUniqueException : Exception
+    {
+        public NotUniqueException(string entity, string property1, int value1, string property2, int value2)
+            : base($"{entity} with {property1} equal {value1} and {property2} equal {value2} already exists.")
+        {
+        }
+    }
+}
--- ELearning.Application/Variants/Commands/CreateVariant/CreateVariantCommandHandler.cs
+++ ELearning.Application/Variants/Commands/CreateVariant/CreateVariantCommandHandler.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading;
+﻿using System;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using ELearning.Application.Exceptions;
 using ELearning.Domain.Entities;
 using ELearning.Persistence;
 using MediatR;
@@ -20,8 +23,15 @@ namespace ELearning.Application.Variants.Commands.CreateVariant
             var entity = new Variant
             {
                 Number = request.Number,
-                ExerciseId = request.ExerciseId
+                ExerciseId = request.ExerciseId,
+                Content = request.Content != null ? request.Content : "No content"
             };
+
+            var variantAlreadyExists = _context.Variants
+                .Any(e => e.ExerciseId == entity.ExerciseId && e.Number == entity.Number);
+
+            if (variantAlreadyExists)
+                throw new NotUniqueException(nameof(Variant), nameof(Variant.ExerciseId), request.ExerciseId, nameof(Variant.Number), request.Number);
 
             _context.Variants.Add(entity);
 
