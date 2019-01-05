c89d27b681df943105ab9d25b6e7943ba0d75c94
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-05 03:00:27 +0100

    Add AuthData

---
+++ ELearning.Common/AuthData.cs
@@ -0,0 +1,10 @@
+﻿namespace ELearning.Common
+{
+    public class AuthData
+    {
+        public string Token { get; set; }
+        public long TokenExpirationTime { get; set; }
+        public string Id { get; set; }
+        public string Role { get; set; }
+    }
+}
