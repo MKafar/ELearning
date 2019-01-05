a7eeb47e1a9ff03688a1cae0d23170f33bf44864
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-06 00:31:25 +0100

    Add InvalidPassword custom exception

---
+++ ELearning.Application/Exceptions/InvalidPasswordException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ELearning.Application.Exceptions
+{
+    public class InvalidPasswordException : Exception
+    {
+        public InvalidPasswordException(string user)
+            : base($"Password for user {user} is invalid.")
+        {
+        }
+    }
+}
