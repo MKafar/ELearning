f0899d9f5fa3498a6c0085eeee14609fd411fe49
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-06 00:55:03 +0100

    Change NotUniqueException. Now more generic

--- ELearning.Application/Exceptions/NotUniqueException.cs
+++ ELearning.Application/Exceptions/NotUniqueException.cs
@@ -4,7 +4,7 @@ namespace ELearning.Application.Exceptions
 {
     public class NotUniqueException : Exception
     {
-        public NotUniqueException(string entity, string property1, int value1, string property2, int value2)
+        public NotUniqueException(string entity, string property1, object value1, string property2, object value2)
             : base($"{entity} with {property1}: {value1} and {property2}: {value2} already exists.")
         {
         }
