d038ac74f8668d8e14f1d9b7984f3470d1bcfd60
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-06 15:09:11 +0100

    Add endpoint api/compiler/send to save assignments solution

---
+++ ELearning.Application/Compiler/Commands/SaveAssignmentSolution/SaveAssignmentSolutionCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace ELearning.Application.Compiler.Commands.SaveAssignmentSolution
+{
+    public class SaveAssignmentSolutionCommandValidator : AbstractValidator<SaveAssignmentSolutionCommand>
+    {
+        public SaveAssignmentSolutionCommandValidator()
+        {
+            RuleFor(v => v.AssignmentId)
+                .NotEmpty()
+                .GreaterThan(0);
+
+            RuleFor(v => v.Solution)
+                .NotEmpty();
+        }
+    }
+}
--- ELearning.WebUI/Controllers/CompilerController.cs
+++ ELearning.WebUI/Controllers/CompilerController.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net;
+using System.Threading.Tasks;
 using ELearning.Application.Compiler.Commands.CompileCode;
+using ELearning.Application.Compiler.Commands.SaveAssignmentSolution;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ELearning.WebUI.Controllers
@@ -7,11 +9,21 @@ namespace ELearning.WebUI.Controllers
     [ApiController]
     public class CompilerController : BaseController
     {
-        // POST: api/Compiler
-        [HttpPost]
+        // POST: api/Compiler/run
+        [HttpPost("run")]
         public async Task<ActionResult<string>> Run([FromBody] CompileCodeCommand command)
         {
             return Ok(await Mediator.Send(command));
+        }
+
+        // POST: api/Compiler/send
+        [HttpPost("send")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public async Task<IActionResult> Send([FromBody] SaveAssignmentSolutionCommand command)
+        {
+            await Mediator.Send(command);
+
+            return NoContent();
         }
     }
 }
