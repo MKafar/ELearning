c9a36afb8f7992dc3e921be87e5b94ac169c8b46
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-07 11:30:53 +0100

    Add endpoint GetStudentsList to UserController

---
+++ ELearning.Application/Users/Queries/GetStudentsList/GetStudentsListQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ELearning.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ELearning.Application.Users.Queries.GetStudentsList
+{
+    public class GetStudentsListQueryHandler : IRequestHandler<GetStudentsListQuery, StudentsListViewModel>
+    {
+        private readonly ELearningDbContext _context;
+
+        public GetStudentsListQueryHandler(ELearningDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<StudentsListViewModel> Handle(GetStudentsListQuery request, CancellationToken cancellationToken)
+        {
+            var vm = new StudentsListViewModel
+            {
+                Students = await _context.Users
+                    .Select(e => new StudentLookupModel
+                    {
+                        Id = e.UserId,
+                        Name = $"{e.Name} {e.Surname}",
+                        Email = e.Email,
+                        RoleId = e.RoleId
+                    }).Where(e => e.RoleId == 1)
+                    .ToListAsync(cancellationToken)
+            };
+
+            return vm;
+        }
+    }
+}
---
+++ ELearning.Application/Users/Queries/GetStudentsList/StudentsListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ELearning.Application.Users.Queries.GetStudentsList
+{
+    public class StudentsListViewModel
+    {
+        public IList<StudentLookupModel> Students { get; set; }
+    }
+}
