d6f62fb8d03d6016a4d0a45fd567d470c5af1fd5
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-08 23:16:14 +0100

    Add query GetUserPastAssignmentsListById

---
+++ ELearning.Application/Users/Queries/GetUserPastAssignmentsListById/UserPastAssignmentLookupModel.cs
@@ -0,0 +1,20 @@
+﻿namespace ELearning.Application.Users.Queries.GetUserPastAssignmentsListById
+{
+    public class UserPastAssignmentLookupModel
+    {
+        public int AssignmentId { get; set; }
+        public int UserId { get; set; }
+        public string Date { get; set; }
+        public string Solution { get; set; }
+        public double? FinalGrade { get; set; }
+
+        public int ExerciseId { get; set; }
+        public string ExerciseTitle { get; set; }
+
+        public int VariantId { get; set; }
+        public string Content { get; set; }
+
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+    }
+}
