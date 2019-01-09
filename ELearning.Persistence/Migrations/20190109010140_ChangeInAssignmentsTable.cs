3b66d326cb0c752fdd8355c50a71d7afb7b73d82
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-09 02:26:15 +0100

    Add new migrations

---
+++ ELearning.Persistence/Migrations/20190109010140_ChangeInAssignmentsTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ELearning.Persistence.Migrations
+{
+    public partial class ChangeInAssignmentsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Assignments",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Assignments",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+        }
+    }
+}
