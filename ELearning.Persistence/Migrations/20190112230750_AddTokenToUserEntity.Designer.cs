461457b46ea93d90f38f3b5b1d05856b0086ab0a
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-13 00:20:04 +0100

    Update database to include Token property in User entity

--- ELearning.Persistence/Migrations/20190112230750_AddTokenToUserEntity.Designer.cs
+++ ELearning.Persistence/Migrations/20190112230750_AddTokenToUserEntity.Designer.cs
@@ -4,14 +4,16 @@ using ELearning.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ELearning.Persistence.Migrations
 {
     [DbContext(typeof(ELearningDbContext))]
-    partial class ELearningDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190112230750_AddTokenToUserEntity")]
+    partial class AddTokenToUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ELearning.Persistence/Migrations/20190112230750_AddTokenToUserEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ELearning.Persistence.Migrations
+{
+    public partial class AddTokenToUserEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Token",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Token",
+                table: "Users");
+        }
+    }
+}
