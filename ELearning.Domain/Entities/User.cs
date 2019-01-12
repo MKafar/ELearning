6eee1ccd5fd0cbedf779cd01aa2750abd21bfdb4
Mateusz <mm.mateusz.mlotek@gmail.com>
2019-01-13 00:19:31 +0100

    Added Token property to User entity.

--- ELearning.Domain/Entities/User.cs
+++ ELearning.Domain/Entities/User.cs
@@ -15,6 +15,7 @@ namespace ELearning.Domain.Entities
         public string Email { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
+        public string Token { get; set; }
 
         public int RoleId { get; set; }
         public Role Role { get; set; }
